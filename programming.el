52edc87c0487c2af48e12a0ced3db2332141929c
Valery Yushchenko <valery.yushchenko@gmail.com>
2010-09-25 19:43:00 +0300

    CoffeeScript mode added

--- programming.el
+++ programming.el
@@ -4,6 +4,20 @@
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (setq js2-bounce-indent-p t)
 
+
+;; CoffeeScript >>>
+
+(require 'coffee-mode)
+(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
+(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
+
+(defun coffee-custom ()
+  "coffee-mode-hook"
+ (set (make-local-variable 'tab-width) 2))
+
+(add-hook 'coffee-mode-hook
+  '(lambda() (coffee-custom)))
+
 ;; CSharp  >>>
 
 (require 'csharp-mode)
