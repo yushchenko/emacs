aa209dde772143579165c978ff19445906151e6e
Valery Yushchenko <valery.yushchenko@gmail.com>
2010-11-08 09:10:31 +0200

    linux font set up

--- system-linux.el
+++ system-linux.el
@@ -1,5 +1,5 @@
 
 (custom-set-faces
- '(default ((t (:inherit nil :stipple nil :background "#141414" :foreground "#F8F8F8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 112 :width normal :foundry "unknown" :family "Monospace")))))
+ '(default ((t (:inherit nil :stipple nil :background "#141414" :foreground "#F8F8F8" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 111 :width normal :foundry "unknown" :family "Monospace")))))
 
 (provide 'system-linux)
