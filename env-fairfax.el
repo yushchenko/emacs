f47c07f8e3c6116f9495442d3d5b9a0aca2a16ae
Valerii Iushchenko <viushchenko@fairfaxdigital.com.au>
2012-02-28 16:56:01 +1100

    not used utils.el removed, configuration specific for Fairfax added

---
+++ env-fairfax.el
@@ -0,0 +1,9 @@
+;; Fairfax Digital - 2012
+
+(if (window-system)
+  (set-frame-height (selected-frame) 75))
+
+(if (window-system)
+  (set-frame-width (selected-frame) 100))
+
+(provide 'env-fairfax)
--- init.el
+++ init.el
@@ -1,9 +1,8 @@
 ;; Valery Yushchenko's Emacs configuration
 ;;
-;; C:\emacs\bin\runemacs.exe -q -l d:\home\projects\emacs\init.el
-;;
-;; /usr/share/applications/emacs23.desktop
-;; Exec=/usr/bin/emacs23 -q -l /home/valery/projects/emacs/init.el %F
+;; .emacs
+;; (add-to-list 'load-path "~/projects/emacs")
+;; (require 'init)
 
 (setq user-full-name "Valery Yushchenko")
 (setq user-mail-address "valery[at]yushchenko.name")
@@ -17,7 +16,6 @@
 
 
 (require 'common)
-(require 'utils)
 (require 'programming)
 (require 'projects)
 
